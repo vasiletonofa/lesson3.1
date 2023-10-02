0cf7660095e7f3f93573d9b0975ecf357e1bad96
vasile <vasile.tonofa@gmail.com>
2023-10-02 20:35:41 +0300

    change main method

--- src/Automobil.java
+++ src/Automobil.java
@@ -1,14 +1,34 @@
 public class Automobil {
 
-    public Integer anulProducerii;
+    public Integer anulProducerii; // null
 
-    public Integer km;
+    public int km; // 0
+
+    String denumire;
+
+    String descriere;
+
 
     public Automobil(Integer anul, Integer kilometraj) {
         anulProducerii = anul;
         km = kilometraj;
     }
 
+
+
+    public Automobil(Integer anul, Integer kilometraj, String desc, String denumire) {
+        anulProducerii = anul;
+        km = kilometraj;
+        descriere = desc;
+    }
+
+    public Automobil(Integer anul) {
+        anulProducerii = anul;
+    }
+
+    public Automobil(String nume) {
+        denumire = nume;
+    }
 
     public Integer incrementByOne(Integer a) {
        return a + 1;
@@ -29,6 +49,10 @@ public class Automobil {
 
     public int getKm() {
         return km;
+    }
+
+    public void setDescriere(String desc) {
+        descriere = desc;
     }
 
 }
--- src/Main.java
+++ src/Main.java
@@ -1,29 +1,71 @@
+
+import person.Person;
+import person.v2.Person2;
+
 public class Main {
     public static void main(String[] args) {
 
-      Automobil bmw = new Automobil(2005, 111111);
+//      Automobil mercedes = new Automobil("Mercedes");
+//
+//      Automobil mazda = new Automobil(2009, 30000);
+//
+//      Automobil bmw = new Automobil(2005, 111111);
+//
+//      System.out.println(bmw.getAnulProducerii());
+//      System.out.println(bmw.getKm());
+//
+//
+//      bmw.setAnulProducerii(2008);
+//
+//      System.out.println(bmw.getAnulProducerii());
+//      System.out.println(bmw.getKm());
+//
+//
+      String a = "120";
 
-      System.out.println(bmw.getAnulProducerii());
-      System.out.println(bmw.getKm());
+      Short v = new Short(a);
+
+      Integer i = new Integer("22");
+
+      int b = Integer.parseInt("233");
 
 
-      bmw.setAnulProducerii(2008);
+      short ll = Short.parseShort("22");
 
-      System.out.println(bmw.getAnulProducerii());
-      System.out.println(bmw.getKm());
+      short hh = new Short("22");
 
-
-      String a = "2";
-
-      Integer v = new Integer(a);
+      short  e = v;
 
       int x = v.intValue();
 
       short b = v.shortValue();
 
-      Boolean t = new Boolean("true");
+      Boolean t = new Boolean( "false" );
+      boolean c =  t;
+
+      boolean o = false;
 
       boolean r = t.booleanValue();
+
+      Person person = new Person();
+      Person2 person2 = new Person2();
+
+      Integer tt = new Integer(2);
+
+      int nn = tt; // unboxing
+
+      Integer rr = 2; // autoboxing  -> new Integer(2)
+
+      Integer oo = nn; // autoboxing -> new Integer(nn)
+
+      Automobil automobil = new Automobil(2010, 45000);
+
+      System.out.println(automobil.getAnulProducerii());
+      System.out.println(automobil.getKm());
+
+      automobil.setDescriere("Automobilul este practic nou");
+
+      System.out.println(automobil.descriere);
 
     }
 
